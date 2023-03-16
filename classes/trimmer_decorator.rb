7d1965a1589ad418e814dcfdb4d2228696ac765d
Swarzstein <zorrillagja@gmail.com>
2023-03-16 12:25:41 -0400

    Changes applied

--- classes/trimmer_decorator.rb
+++ classes/trimmer_decorator.rb
@@ -3,7 +3,7 @@ require_relative 'decorator'
 class TrimmerDecorator < Decorator
   def correct_name
     name = @nameable.correct_name
-    name = name[0, 9] if name.length > 10
+    name = name[0, 10] if name.length > 10
     name
   end
 end
