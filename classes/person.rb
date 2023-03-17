00bd417c73a3756097766e19714b06107533ddb8
Swarzstein <zorrillagja@gmail.com>
2023-03-17 02:08:25 -0400

    many-to-many relationship between book and person implemented

--- classes/person.rb
+++ classes/person.rb
@@ -1,16 +1,17 @@
 require_relative 'nameable'
 
 class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-
-  attr_accessor :name, :age
-  attr_reader :id
 
   private
 
