704158fce7c8cda768fe5cfdc94665d5e23bce9c
Swarzstein <zorrillagja@gmail.com>
2023-03-17 22:06:12 -0400

    linters fixed

--- app.rb
+++ app.rb
@@ -16,21 +16,19 @@ person3 = Person.new(32, 'Hanna')
 book1 = Book.new('Simon Bolivar', 'Arturo Fuentes')
 book2 = Book.new('Abraham Lincoln', 'Vanessa Lee')
 
-rental = Rental.new('2023-01-01', book1, person)
-rental = Rental.new('2023-01-01', book2, person)
-rental = Rental.new('2023-01-03', book2, person2)
-rental = Rental.new('2023-02-12', book1, person3)
-rental = Rental.new('2023-03-06', book1, person2)
+book1.add_rental(Rental.new('2023-01-01', book1, person))
+book2.add_rental(Rental.new('2023-01-01', book2, person))
+book2.add_rental(Rental.new('2023-01-03', book2, person2))
+person3.add_rental(Rental.new('2023-02-12', book1, person3))
+person2.add_rental(Rental.new('2023-03-06', book1, person2))
 
 p "books rented by #{person.name}:"
-person.rentals.each {|ren| p [ren.date, ren.book.title, ren.book.author]}
+person.rentals.each { |ren| p [ren.date, ren.book.title, ren.book.author] }
 p "books rented by #{person2.name}:"
-person2.rentals.each {|ren| p [ren.date, ren.book.title, ren.book.author]}
+person2.rentals.each { |ren| p [ren.date, ren.book.title, ren.book.author] }
 p "books rented by #{person3.name}:"
-person3.rentals.each {|ren| p [ren.date, ren.book.title, ren.book.author]}
+person3.rentals.each { |ren| p [ren.date, ren.book.title, ren.book.author] }
 p "people who rented #{book1.title} by #{book1.author}:"
-book1.rentals.each {|ren| p [ren.date, ren.person.name]}
+book1.rentals.each { |ren| p [ren.date, ren.person.name] }
 p "people who rented #{book2.title} by #{book2.author}:"
-book2.rentals.each {|ren| p [ren.date, ren.person.name]}
-
-
+book2.rentals.each { |ren| p [ren.date, ren.person.name] }
